892b83b662dffb8b2a45b0f4a5bdcf02cbb5fb1d
Janet Gilbert <janet@wadjeteyegames.com>
2021-02-22 16:02:29 -0500

    Initial

---
+++ Assets/Scripts/RefOut.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ClassString
+{
+    public string name;
+}
+
+public struct StructString
+{
+    public string name;
+}
+
+
+public class RefOut : MonoBehaviour
+{
+
+    void Start()
+    {
+        ClassString myClassString = new ClassString();
+
+
+        StructString myStructString;
+
+
+       myClassString.name = "MyClassString";
+       myStructString.name = "MyStructString";
+
+       Debug.Log("myClassString.name:" + myClassString.name + " myStructString.name" + myStructString.name);
+
+
+        BothTypes(myClassString, out myStructString);
+
+        Debug.Log("AFTER myClassString.name:" + myClassString.name + " myStructString.name" + myStructString.name);
+
+    }
+
+    int BothTypes(ClassString a, out StructString b)
+    {
+        a.name = "Betty";
+        b.name = "Veronica";
+
+
+        return 5;
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spamObject;
+    public int addPerFrame = 5;
+    public float maxDist = 100.0f;
+    private Renderer[] cachedRenderer;
+    public int addAtStart = 10000;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < addAtStart; i++)
+        {
+            Vector3 randomPos = Random.onUnitSphere * Random.Range(0.0f, maxDist);
+
+            Instantiate(spamObject, randomPos, Quaternion.identity, transform);
+
+        }
+
+        cachedRenderer = GetComponentsInChildren<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        MakeTheRainbow();
+        MakeTheRainbowCached();
+    }
+
+    void MakeTheRainbow()
+    {
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer r in renderers)
+        {
+            r.material.color = Random.ColorHSV();
+        }
+    }
+
+    void MakeTheRainbowCached()
+    {
+
+        foreach (Renderer r in cachedRenderer)
+        {
+            r.material.color = Random.ColorHSV();
+        }
+    }
+}
